b19fd21e13722415341f27ef5951c9400d6aa166
codeanywhere <codeanywhere@ibloviate.org>
2024-03-22 15:54:19 -0400

    Refactored day 1a

--- day01a.go
+++ day01a.go
@@ -16,38 +16,60 @@ func getData() []byte {
 	return (inData)
 }
 
-/*func getFirstIntFromString(line string) {
+func getFirstIntFromString(line string) string {
 	if(!(len(line) > 0)) {
 		panic("String too short")
 	}
-	firstInt := line[0][0:1]
+	firstInt := line[0:1]
 	return (firstInt)
-}*/
+}
+
+func getLastIntFromString(line string) string {
+	if(!(len(line) > 0)) {
+		panic("String too short")
+	}
+	lastInt := line[len(line) - 1:]
+	return (lastInt)
+}
+
+func getCalibrationPairFromLine(line string) string{
+	// Calibration Pair is first int and last int in a string
+	re := regexp.MustCompile(`\d+`)
+	vals := re.FindAllString(line, -1)
+
+	first := getFirstIntFromString(vals[0])
+	last := getLastIntFromString(vals[len(vals) - 1])
+	calibrationPair := first + last
+
+	return (calibrationPair)
+}
+
+func getCalibrationValueFromLine(line string) int64 {
+	// Calibration Value is the first and last integer in a line put together to form a 2-digit number
+	calibrationPair := getCalibrationPairFromLine(line)
+					
+	calibrationVal, err := strconv.ParseInt(calibrationPair, 0, 64)
+	if(err != nil) {
+		panic(err)
+	}
+
+	return (calibrationVal)
+}
+
 
 func main() {
 	// Combine the first and last digit from each line to form a value, get running total of all
 	var inData []byte = getData()
-
 	inDataSplit := strings.Split(string(inData), "\n")
+
 	var runningTotal int64 = 0
 
 	for _, line := range inDataSplit {
-			re := regexp.MustCompile(`\d+`)
-			vals := re.FindAllString(line, -1)
-
-			var first string = vals[0][0:1]
-			var last string = vals[len(vals) - 1]
-			last = last[len(last)-1:]
-
-			var lineVal string = first + last
-		
-			calibrationVal, err := strconv.ParseInt(lineVal, 0, 64)
-			if(err != nil) {
-				panic(err)
-			}
+			var calibrationVal int64 = getCalibrationValueFromLine(line)
+			
 			runningTotal += calibrationVal
-			fmt.Println(line, " ", lineVal, " ", calibrationVal, " ", runningTotal)
+			// fmt.Println(line, " ", calibrationVal, " ", runningTotal)
 
 	}
-	fmt.Println(runningTotal)
+	fmt.Println("Calibration Value: ", runningTotal)
 }
